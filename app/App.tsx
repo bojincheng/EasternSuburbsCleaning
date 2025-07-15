333c0cd9573d48238648ce294e57a9d48e158ece
bche0078 <bche0078@student.monash.edu>
2025-07-15 22:33:03 +1000

    simplified some files

--- app/App.tsx
+++ app/App.tsx
@@ -1,47 +1,7 @@
-/* import React from 'react';
-import { Auth0Provider } from '@auth0/auth0-react';
-import { authConfig } from '../constants/authConfig';
-import Dashboard from '../components/Dashboard';
+/*In Expo Router projects, App.tsx is mostly ignored 
+Expo Router auto-generates your app’s navigation from the /app directory.
+The actual entry point becomes app/_layout.tsx, where you wrap your app in providers.
+App.tsx must exist
+*/
+import 'expo-router/entry';
 
-const App = () => {
-    return (
-        <Auth0Provider
-            domain={authConfig.domain}
-            clientId={authConfig.clientId}
-            authorizationParams={authConfig.authorizationParams}
-        >
-            <Dashboard />
-        </Auth0Provider>
-    );
-};
-
-export default App; */
-
-import React from 'react';
-import { Auth0Provider } from '@auth0/auth0-react';
-import Dashboard from '../components/Dashboard';
-/* Load the variables from .env file*/
-const App = () => {
-    const isWeb = typeof window !== 'undefined';
-
-    const domain = process.env.REACT_APP_AUTH0_DOMAIN!;
-    const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID!;
-    const redirectUri = isWeb
-        ? window.location.origin
-        : process.env.REACT_APP_AUTH0_REDIRECT_URI!;
-
-    return (
-        <Auth0Provider
-            domain={domain}
-            clientId={clientId}
-            authorizationParams={{
-                redirect_uri: redirectUri,
-                scope: 'openid profile email',
-            }}
-        >
-            <Dashboard />
-        </Auth0Provider>
-    );
-};
-
-export default App;
